bdd9cbf25ddcb08f35de4c7cbd753428b033159c
KitoCinnamon <110126581+Cyrodwd@users.noreply.github.com>
2024-03-28 18:52:33 -0700

    It just works

---
+++ source/app.d
@@ -0,0 +1,28 @@
+import std.stdio : write, writeln;
+import std.file : readText, FileException;
+import std.exception;
+
+void main(string[] args)
+{
+    if (args.length < 2) {
+        writeln("usage: dog <file>");
+        return;
+    }
+
+    try {
+        loadFile(args[1]);
+    } catch (Exception e) {
+        writeln("ERROR: ", e.msg);
+    }
+}
+
+void loadFile(string path) {
+    try {
+        string fileContent = readText(path);
+        writeln(fileContent);
+    } catch (FileException fe) {
+        writeln("ERROR: File not found or unable to read: ", fe.msg);
+    } catch (Exception e) {
+        writeln("ERROR: ", e.msg);
+    }
+}
